44ae895c9a9cb53e51c2d9c2cf7e4432cc894fbc
felipeog <17603069+felipeog@users.noreply.github.com>
2022-05-31 17:38:14 -0300

    Update connect section

--- scripts/generateReadme/consts/readmeSections/connect.js
+++ scripts/generateReadme/consts/readmeSections/connect.js
@@ -1,6 +1,6 @@
 const connect =
   `## 🤝🏻 Connect with me\n` +
   `\n` +
-  `[![](https://img.shields.io/badge/LinkedIn-Felipe%20OG-blue?logo=linkedin)](https://www.linkedin.com/in/felipeog/)\n`;
+  `[![](https://img.shields.io/badge/LinkedIn-Felipe%20OG-0077b5?logo=linkedin&labelColor=333)](https://www.linkedin.com/in/felipeog/)\n`;
 
 module.exports = connect;
