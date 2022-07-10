24fa8eb473cf416dc31837cc937fc7ed2bed2b28
felipeog <17603069+felipeog@users.noreply.github.com>
2022-07-10 18:16:13 -0300

    Update clone

--- scripts/generateReadme/utils/cloneRepositories.js
+++ scripts/generateReadme/utils/cloneRepositories.js
@@ -2,11 +2,11 @@ import { exec } from "child_process";
 
 function cloneRepository(repositoryName) {
   return new Promise((resolve, reject) => {
-    const [user, repository] = repositoryName.split("/");
+    // const [user, repository] = repositoryName.split("/");
 
     exec(
-      // `git clone https://github.com/${repositoryName}.git repositories/${repositoryName} --depth 1`,
-      `git clone git@github.com:${user}/${repository} repositories/${repositoryName} --depth 1`,
+      `git clone https://github.com/${repositoryName}.git repositories/${repositoryName} --depth 1`,
+      // `git clone git@github.com:${user}/${repository} repositories/${repositoryName} --depth 1`,
       (error, stdout) => {
         if (error) {
           reject(`error: ${error.message}`);
