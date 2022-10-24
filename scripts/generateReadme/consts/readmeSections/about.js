2c61fa47005f2df7b9514997cf1f0d5aff202dd7
felipeog <17603069+felipeog@users.noreply.github.com>
2022-10-23 23:36:23 -0300

    Update about

--- scripts/generateReadme/consts/readmeSections/about.js
+++ scripts/generateReadme/consts/readmeSections/about.js
@@ -1,4 +1,6 @@
 export const about =
   `## 👨🏻‍💻 About me\n` +
   `\n` +
-  `I'm working as a Frontend Developer at Winnin.\n`;
+  `I'm working as a Frontend Developer at [Winnin](https://winnin.com/en/).\n`
+  `\n` +
+  `Sometimes I write about Web Development on [my blog](https://blog.felipeog.com.br/en/).\n`;
